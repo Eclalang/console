32adf5c5aab02ec182c0b281685e773277e41ff1
mozaBit <bamhammedmetehri@gmail.com>
2022-11-30 15:47:03 +0000

    finished consolePackage v1

---
+++ ConsoleLibTester/ConsoleLibTester_test.go
@@ -0,0 +1,18 @@
+package consolelibtester
+
+import (
+	_ "github.com/Eclalang/console"
+	_ "strconv"
+	"testing"
+)
+
+func TestInput(t *testing.T) {
+	// input := console.Input("Enter your name: ")
+	// console.Println("Hello", input)
+	// input = console.Input("Enter your age: ")
+	// age, err := strconv.Atoi(input)
+	// if err != nil {
+	// 	t.Error("Error converting age to int")
+	// }
+	// console.Println("You born in", 2022-age, "and you are", input, "years old")
+}
--- console.go
+++ console.go
@@ -1,21 +1,54 @@
 package console
 
 import (
+	"bufio"
 	"fmt"
-	_ "os"
+	"os"
 )
 
 // Function to print args as formated string to console
-func printf(format string, args ...interface{}) {
+func Printf(format string, args ...interface{}) {
 	fmt.Printf(format, args...)
 }
 
 // Function to print args to console
-func println(args ...interface{}) {
+func Println(args ...interface{}) {
 	fmt.Println(args...)
 }
 
 // Function to print args to console withou newline
-func print(args ...interface{}) {
+func Print(args ...interface{}) {
 	fmt.Print(args...)
+}
+
+// Function that take user input from console
+func Input(args ...interface{}) string {
+	var input string
+	fmt.Print(args...)
+	reader := bufio.NewReader(os.Stdin)
+	input, _ = reader.ReadString('\n')
+	return input
+}
+
+func PrintInColor(color string, args ...interface{}) {
+	switch color {
+	case "red":
+		fmt.Print("\033[31m")
+	case "green":
+		fmt.Print("\033[32m")
+	case "yellow":
+		fmt.Print("\033[33m")
+	case "blue":
+		fmt.Print("\033[34m")
+	case "magenta":
+		fmt.Print("\033[35m")
+	case "cyan":
+		fmt.Print("\033[36m")
+	case "white":
+		fmt.Print("\033[37m")
+	case "reset":
+		fmt.Print("\033[0m")
+	}
+	fmt.Print(args...)
+	fmt.Print("\033[0m")
 }
